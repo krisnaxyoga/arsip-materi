435b4355b9fe76fad0b3ef1ff23332ce543ff849
krisnaxyoga <yogakris.yk@gmail.com>
2023-07-09 11:51:01 +0700

    update database

---
+++ database/migrations/2023_07_09_040850_create_komentars_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('komentars', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('anggota_id');
+            $table->unsignedBigInteger('petugas_id');
+            $table->unsignedBigInteger('materi_id');
+            $table->text('comment')->nullable();
+            $table->string('status')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('komentars');
+    }
+};
